bbc4ed6d41a40ccaf24cada595bc4ddf22b9bb1c
elvynmejia <elvynmejia@gmail.com>
2020-11-04 09:49:35 -0800

    Adds commands to setup mysql and node

--- install.sh
+++ install.sh
@@ -2,9 +2,30 @@
 
 echo ">>> Starting Install Script"
 sudo apt-get update -y
+sudo apt-get install -y curl
 sudo apt-get install -y git
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update -y
 sudo apt-get install -y python3.6
 sudo apt-get install -y python3.6-venv
+
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+
+# Setting MySQL root user password root/root
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+
+
+# Installing packages
+sudo apt-get install -y mysql-server mysql-client
+
+# Allow External Connections on your MySQL Service
+# sudo sed -i -e 's/bind-addres/#bind-address/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+# sudo sed -i -e 's/skip-external-locking/#skip-external-locking/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root'; FLUSH privileges;"
+sudo service mysql restart
+# create client database
+# mysql -u root -proot -e "CREATE DATABASE vmdb;"
